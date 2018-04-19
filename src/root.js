fa229b267c00ed95864f6e6fc45c9511081352c8
slice <jianghenchao@sina.com>
2018-04-19 22:51:37 +0800

    修改login

--- src/root.js
+++ src/root.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import {Route,BrowserRouter} from 'react-router-dom';
+import {Route,BrowserRouter,Link} from 'react-router-dom';
 import {Button} from 'antd';
 import 'antd/dist/antd.css';
 import './commons/css/root.css'
@@ -11,9 +11,8 @@ class Root extends Component{
   render(){
     return(
       <div className="root">       
-        <BrowserRouter>
-          <Route path="/" component={userLogin}></Route>
-          
+        <BrowserRouter basename="/index">
+          <Link path="/login" component={userLogin}></Link>          
         </BrowserRouter>     
       </div>
     )
