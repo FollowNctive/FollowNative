9f3db457f7673b3f1b73e09142159c3a2cbea68a
slice <jianghenchao@sina.com>
2018-04-19 20:51:29 +0800

    login页面

--- src/components/login.js
+++ src/components/login.js
@@ -19,28 +19,50 @@ class Login extends Component{
 
     render(){
       const { getFieldDecorator } = this.props.form;
-
+      const FormItemLayout = {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 4 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 20 },
+        }
+      }
       return(
         <div className="login">
-           <Row>             
-             <Col span={4} offset="10">
+           
               <Form>
-                <FormItem label="user">
+                <FormItem className="form-title">
+                  <h1>管理员登录</h1>
+                </FormItem>
+                <FormItem 
+                {...FormItemLayout}                
+                label="用户名:">
                 {getFieldDecorator('userName', {
               rules: [{ required: true, message: '请输入你的账号' }],
             })(
               <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="账号" />
             )}
                 </FormItem>
-                <FormItem>
+                <FormItem 
+                {...FormItemLayout}
+                label="密码"
+                >
                   {getFieldDecorator('userPassword',{
                     rules:[{required:true,message:'请输入密码'}]
                   })(<Input prefix={<Icon type="lock"/>} type="password" placeholder="请输入密码"/>)}
                 </FormItem>
               </Form>
-              <Button type="button">登录</Button>
-             </Col>   
-           </Row>
+              <FormItem
+          wrapperCol={{
+            xs: { span: 22, offset: 2 },
+            sm: { span: 20, offset: 4 },
+          }}
+        >
+          <Button type="primary" htmlType="submit" className="login-form">Submit</Button>
+        </FormItem>
+           
            
         </div>
       )
